b50d9bc71d8bdfb16199d2cf62b51ee3b0015ebf
Hedron  Dantas <hedrondantas@gmail.com>
2025-05-06 15:28:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,4 @@ sudo cp apt-manager.sh /usr/local/bin/apt-manager
 # make script executable
 sudo chmod +x /usr/local/bin/apt-manager
 
-# create terminal alias
-echo "alias apt-manager='bash /usr/local/bin/apt-manager'" >> ~/.bashrc
-
-echo "Installation complete. Please restart your terminal or run 'source ~/.bashrc' to use the alias."
+echo "Installation complete. Please restart your terminal."
